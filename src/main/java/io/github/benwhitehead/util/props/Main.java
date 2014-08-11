575964f4ecfa8b690fcfaaeb894292c4f31ac37f
BenWhitehead <benjamin.r.whitehead@gmail.com>
2014-08-10 22:11:34 -0700

    Update pom and the versions of dependencies in it

---
+++ src/main/java/io/github/benwhitehead/util/props/Main.java
@@ -0,0 +1,10 @@
+package io.github.benwhitehead.util.props;
+
+/**
+ * @author Ben Whitehead
+ */
+public final class Main {
+    public static void main(String[] args) {
+        System.out.println("props");
+    }
+}
