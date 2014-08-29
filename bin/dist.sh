46e3172e7eaaf2f6a62d40d494e86666c4224e48
BenWhitehead <benjamin.r.whitehead@gmail.com>
2014-08-29 15:48:47 -0700

    Add scripts of making an executable

---
+++ bin/dist.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Creates a schell script with an attached binary
+# @see: https://coderwall.com/p/ssuaxa
+
+TARGET=../target
+OF=${TARGET}/props
+
+cat props.sh ${TARGET}/java-props-*.jar > ${OF}
+chmod +x ${OF}
